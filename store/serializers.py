8a4bb2d08479fa68fd0e87a7f8979c749f336c37
Emily Gavrilenko <egavrile@calpoly.edu>
2023-09-11 22:16:55 -0700

    POST and PUT endpoints

--- store/serializers.py
+++ store/serializers.py
@@ -2,23 +2,28 @@ from rest_framework import serializers
 from .models import Product, Collection
 from decimal import Decimal
 
-class CollectionSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    title = serializers.CharField(max_length=255)
+class CollectionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ['id', 'title']
 
 
-class ProductSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    title = serializers.CharField(max_length=255)
-    price = serializers.DecimalField(max_digits=6, decimal_places=2, source='unit_price')
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ['id', 'title', 'description', 'slug', 'unit_price', 'price_with_tax', 'inventory', 'collection']
+    # id = serializers.IntegerField()
+    # title = serializers.CharField(max_length=255)
+    # price = serializers.DecimalField(max_digits=6, decimal_places=2, source='unit_price')
     price_with_tax = serializers.SerializerMethodField(method_name='calculate_tax')
     # collection = CollectionSerializer()
-    collection = serializers.HyperlinkedRelatedField(
-        queryset=Collection.objects.all(),
-        view_name='collection-detail',
-        lookup_field='id'
-)
+
+    # Hyperlink related field
+    # collection = serializers.HyperlinkedRelatedField(
+    #     queryset=Collection.objects.all(),
+    #     view_name='collection-detail',
+    #     lookup_field='id'
+    # )
 
     def calculate_tax(self, product: Product):
-        return product.unit_price * Decimal(1.1)
-    
+        return product.unit_price * Decimal(1.1)    
--- store/views.py
+++ store/views.py
@@ -5,17 +5,29 @@ from rest_framework.response import Response
 from .models import Product, Collection
 from .serializers import ProductSerializer, CollectionSerializer
 
-@api_view()
+@api_view(['GET', 'POST'])
 def product_list(request):
-    product_set = Product.objects.select_related('collection').all()
-    serializer = ProductSerializer(product_set, many=True, context={'request': request})
-    return Response(serializer.data)
+    if request.method == 'GET':
+        product_set = Product.objects.select_related('collection').all()[:10]
+        serializer = ProductSerializer(product_set, many=True, context={'request': request})
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = ProductSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=201)
 
-@api_view()
+@api_view(['GET', 'PUT'])
 def product_detail(request, id):
     product = get_object_or_404(Product, pk=id)
-    serializer = ProductSerializer(product)
-    return Response(serializer.data)
+    if request.method == 'GET':
+        serializer = ProductSerializer(product)
+        return Response(serializer.data)
+    elif request.method == 'PUT':
+        serializer = ProductSerializer(product, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
 
 @api_view()
 def collection_detail(request, id):
