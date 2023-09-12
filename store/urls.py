638e61c1ba77221bea54b7798e17b321f74ef44d
Emily Gavrilenko <egavrile@calpoly.edu>
2023-09-12 10:25:48 -0700

    Endpoint to get cart items

--- store/urls.py
+++ store/urls.py
@@ -4,16 +4,25 @@ from pprint import pprint
 from rest_framework_nested import routers
 
 router = routers.DefaultRouter()
-router.register('products', views.ProductViewSet, basename='products')
 router.register('collections', views.CollectionViewSet)
-router.register('carts', views.CartViewSet)
 
+# Products
+router.register('products', views.ProductViewSet, basename='products')
+# Products/Reviews nested router
 product_router = routers.NestedDefaultRouter(router, 'products', lookup='product')
 product_router.register('reviews', views.ReviewViewSet, basename='product-reviews')
+
+# Carts
+router.register('carts', views.CartViewSet)
+# Cart items nested router
+carts_router = routers.NestedDefaultRouter(router, 'carts', lookup='cart')
+carts_router.register('items', views.CartItemViewSet, basename='cart-items')
+
 
 # URLConf
 # urlpatterns = router.urls
 urlpatterns = [
     path('', include(router.urls)),
-    path('', include(product_router.urls))
+    path('', include(product_router.urls)),
+    path('', include(carts_router.urls))
 ]
--- store/views.py
+++ store/views.py
@@ -3,12 +3,12 @@ from django.db.models import Count
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.viewsets import ModelViewSet, GenericViewSet
-from rest_framework.mixins import CreateModelMixin, RetrieveModelMixin, DestroyModelMixin
+from rest_framework.mixins import CreateModelMixin, RetrieveModelMixin, DestroyModelMixin, ListModelMixin
 from rest_framework.filters import SearchFilter, OrderingFilter
 from rest_framework.pagination import PageNumberPagination
 from django_filters.rest_framework import DjangoFilterBackend
-from .models import Product, Collection, OrderItem, Review, Cart
-from .serializers import ProductSerializer, CollectionSerializer, ReviewSerializer, CartSerializer
+from .models import Product, Collection, OrderItem, Review, Cart, CartItem
+from .serializers import ProductSerializer, CollectionSerializer, ReviewSerializer, CartSerializer, CartItemSerializer
 from .filters import ProductFilter
 from .pagination import DefaultPagination
 
@@ -57,3 +57,13 @@ class ReviewViewSet(ModelViewSet):
 class CartViewSet(CreateModelMixin, RetrieveModelMixin, DestroyModelMixin, GenericViewSet): # Only supports the create operation
     queryset = Cart.objects.prefetch_related('items__product').all()
     serializer_class = CartSerializer 
+
+
+class CartItemViewSet(ListModelMixin, RetrieveModelMixin, GenericViewSet):
+    serializer_class = CartItemSerializer
+
+    def get_queryset(self):
+        return CartItem.objects.filter(cart_id=self.kwargs['cart_pk']).select_related('product')
+
+    def get_serializer_context(self):
+        return {'cart_pk': self.kwargs['cart_pk'] }
