8a4be6f62c23b44bce49a4092347835deb21234a
Emily Gavrilenko <egavrile@calpoly.edu>
2023-09-11 21:57:08 -0700

    Begin creating API enddpoints for the storefront

--- store/views.py
+++ store/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from django.http import HttpResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Product, Collection
+from .serializers import ProductSerializer, CollectionSerializer
 
-# Create your views here.
+@api_view()
+def product_list(request):
+    product_set = Product.objects.select_related('collection').all()
+    serializer = ProductSerializer(product_set, many=True, context={'request': request})
+    return Response(serializer.data)
+
+@api_view()
+def product_detail(request, id):
+    product = get_object_or_404(Product, pk=id)
+    serializer = ProductSerializer(product)
+    return Response(serializer.data)
+
+@api_view()
+def collection_detail(request, id):
+    collection = get_object_or_404(Collection, pk=id)
+    serializer = CollectionSerializer(collection)
+    return Response(serializer.data)
